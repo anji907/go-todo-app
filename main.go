80c121eef0936bccdbb870a6e1c4ba7356b818a1
anji907 <anji.sk129@gmail.com>
2025-06-15 17:46:43 +0900

    🧑🏻‍💻 Add air configuration and Implement GET request handler.

--- main.go
+++ main.go
@@ -7,9 +7,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
+	// 文字列出力
 	fmt.Println("Hello world.")
 
 	app := fiber.New()
+
+	// handle GET request.
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "Hello world!"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
