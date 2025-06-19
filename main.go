cdf0822daf61a30726acd1c08563796b1bfcbeee
anji907 <anji.sk129@gmail.com>
2025-06-20 00:09:58 +0900

    Implement post request handler.

--- main.go
+++ main.go
@@ -19,9 +19,31 @@ func main() {
 
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	// handle GET request.
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"message": "Hello world!"})
+	})
+
+	// handle POST request.
+	app.Post("api/todos", func(c *fiber.Ctx) error {
+		// request body.
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(4000).JSON(fiber.Map{"error": "Todo Body is required."})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+
 	})
 
 	log.Fatal(app.Listen(":4000"))
